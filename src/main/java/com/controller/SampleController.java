05ede8f64a0a02e02b46bff04876f5a1b231bf18
藤原 脩平 <fujiwara_shuhei_gn@cyberagent.co.jp>
2017-04-18 11:33:22 +0900

    add login function

--- src/main/java/com/controller/SampleController.java
+++ src/main/java/com/controller/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class SampleController {
@@ -16,6 +17,11 @@ public class SampleController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView show() {
 		return new ModelAndView("index");
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ModelAndView login() {
+		return new ModelAndView(new RedirectView("/"));
 	}
 
 }
